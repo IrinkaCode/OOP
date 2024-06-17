86184232e90aa653d89ee776ba35cbeb74e1047f
IrinkaCode <pil02022012@gmail.com>
2024-06-17 22:59:02 +0200

    17.06

---
+++ Lesson2(DZ)/HomeLibrary.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_DZ_
+{
+    public class Book
+    {
+        public string ?Title { get; set; }
+        public string ?Author { get; set; }
+        public int Year { get; set; }
+    }
+    internal class HomeLibrary
+    {
+        private List<Book> books;
+
+        public HomeLibrary()
+        {
+            books = new List<Book>();
+        }
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        public void RemoveBook(Book book)
+        {
+            books.Remove(book);
+        }
+
+        public List<Book> FindByAuthor(string author)
+        {
+            return books.Where(b => b.Author == author).ToList();
+        }
+
+        public List<Book> FindByYear(int year)
+        {
+            return books.Where(b => b.Year == year).ToList();
+        }
+
+        public void SortByTitle()
+        {
+            books.Sort((a, b) => a.Title!.CompareTo(b.Title));
+        }
+
+        public void SortByAuthor()
+        {
+            books.Sort((a, b) => a.Author!.CompareTo(b.Author));
+        }
+
+        public void SortByYear()
+        {
+            books.Sort((a, b) => a.Year.CompareTo(b.Year));
+        }
+
+        public void PrintLibrary()
+        {
+            foreach (var book in books)
+            {
+                Console.WriteLine($"Title: {book.Title}, Author: {book.Author}, Year: {book.Year}");
+            }
+        }
+    }
+}
---
+++ Lesson2(DZ)/Program.cs
@@ -0,0 +1,58 @@
+﻿//Создать класс, содержащий динамический массив и количество элементов в нем. Добавить конструктор, который выделяет память под заданное количество элементов, и деструктор. Добавить методы, позволяющие заполнять массив случайными числами, переставлять в данном массиве элементы в случайном порядке, находить количество различных элементов в массиве, выводить массив на экран.
+
+//using Lesson2_DZ_;
+
+//Massiv massiv = new Massiv(10);
+//massiv.Fill();
+//massiv.Print();
+
+//massiv.Mix();
+//massiv.Print();
+
+//Console.WriteLine(massiv.Different());
+
+//Описать класс «домашняя библиотека». Предусмотреть возможность работы с произвольным числом книг, поиска книги по какому-либо признаку (например, по автору или по году издания), добавления книг в библиотеку, удаления книг из нее, сортировки книг по разным полям.
+
+using Lesson2_DZ_;
+
+var library = new HomeLibrary();
+
+var book1 = new Book { Title = "Буратино", Author = "Толстой", Year = 2021 };
+var book2 = new Book { Title = "Незнайка", Author = "Носов", Year = 2020 };
+var book3 = new Book { Title = "Дюймовочка", Author = "Андерсен", Year = 2018 };
+
+library.AddBook(book1);//добавление книг
+library.AddBook(book2);
+library.AddBook(book3);
+
+Console.WriteLine("Библиотека отсортированная по названию:");
+library.SortByTitle();
+library.PrintLibrary();
+
+Console.WriteLine("\nБиблиотека отсортированная по автору:");
+library.SortByAuthor();
+library.PrintLibrary();
+
+Console.WriteLine("\nБиблиотека отсортированная по году:");
+library.SortByYear();
+library.PrintLibrary();
+
+Console.WriteLine("\nКнига от автора 1:");//поиск книги по автору
+var authorBooks = library.FindByAuthor("Носов");
+
+foreach (var book in authorBooks)
+{
+    Console.WriteLine($"Название: {book1.Title}, автор: {book.Author}, год: {book1.Year}");
+}
+
+Console.WriteLine("\nКнига опубликованная в 2020:");//поиск книги по году
+var yearBooks = library.FindByYear(2020);
+foreach (var book in yearBooks)
+{
+    Console.WriteLine($"Название: {book.Title}, автор: {book.Author}");
+}
+Console.WriteLine();
+
+library.RemoveBook(book2);//удаление книги
+library.PrintLibrary();
+    
