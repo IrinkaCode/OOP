b5923e27fa2e83de2885a6ca45cc3711d5eeed39
IrinkaCode <pil02022012@gmail.com>
2024-07-04 16:04:37 +0200

    29.06

---
+++ Lesson9/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson9
+{
+    public delegate void AccountHandler(string message);
+
+    internal class Account
+    {
+        private int sum;
+        AccountHandler? taken;
+        public Account(int s) => this.sum = s;
+        public void RegisterHandler(AccountHandler del)
+        {
+            taken += del;
+        }
+        public void UnRegisterHandler(AccountHandler del)
+        {
+            taken -= del;
+        }
+        public void Add(int sum) { this.sum += sum;
+            taken?.Invoke($"Добавлено {sum}.Баланс {this.sum} у.е.");
+        }
+        public void Take(int sum)
+        {
+            if (this.sum >= sum)
+            {
+                this.sum -= sum;
+                //Console.WriteLine($"Со счета списано {sum} у.е.");
+                taken?.Invoke($"Со счета списано {sum} у.е.Баланс {this.sum} у.е.");
+            }
+            else
+            {
+                taken?.Invoke($"Недостаточно средств.Баланс {this.sum} у.е.");
+            }
+        }
+        
+    }
+}
