aef36f89ffcfffc92443f6762fdf4a70a9531005
IrinkaCode <pil02022012@gmail.com>
2024-07-05 15:49:04 +0200

    04.07-2

---
+++ Lesson10(DZ)/DerivedClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10_DZ_
+{
+    public class DerivedClass
+    {
+        // Второе открытое целочисленное поле
+        public int AnotherValue;
+
+        // Переопределенный индексатор с двумя индексами
+        public override int this[int field, int index]
+        {
+            get
+            {
+                int value = (field == 0) ? Value : AnotherValue;
+                return (int)Math.Pow(10, index) * (value / (int)Math.Pow(10, index)) % 10;
+            }
+        }
+
+        // Переопределенный индексатор с одним индексом
+        public override int this[int index]
+        {
+            get
+            {
+                return (this[0, index] + this[1, index]);
+            }
+        }
+    }
+}
---
+++ Lesson10(DZ)/NumberClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10_DZ_
+{
+    public class NumberClass
+    {
+        // Открытое целочисленное поле
+        public int Value;
+
+        // Виртуальный индексатор
+        public virtual int this[int index]
+        {
+            get
+            {
+                // Получение цифры в десятичном представлении числа
+                return (int)Math.Pow(10, index) * (Value / (int)Math.Pow(10, index)) % 10;
+            }
+        }
+    }
+}
