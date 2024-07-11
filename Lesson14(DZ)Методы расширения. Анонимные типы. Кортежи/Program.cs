a3166f82c703e43c9488ca6267f3a282476ae1e7
IrinkaCode <pil02022012@gmail.com>
2024-07-11 17:17:23 +0200

    11,07

---
+++ Lesson14(DZ)Методы расширения. Анонимные типы. Кортежи/Program.cs
@@ -0,0 +1,39 @@
+﻿//Создайте запись (Record) с именем student, содержащую поля: фамилия и инициалы, номер группы, успеваемость (массив из пяти элементов). Создать массив из десяти элементов такого типа, упорядочить записи по возрастанию среднего балла. Добавить возможность вывода фамилий и номеров групп студентов, имеющих оценки, равные только 4 или 5.
+//using System;
+
+//record Student(string FullName, int GroupNumber, int[] Grades)
+//{
+//    public double AverageGrade => Grades.Average();
+//}
+
+using Lesson14_DZ_Методы_расширения;
+using Lesson14_DZ_Методы_расширения._Анонимные_типы._Кортежи_;
+
+Student[] students = new Student[10]
+{
+            new Student("Иванов И.И.", 1, new int[]{5, 4, 5, 3, 4}),
+            new Student("Петров П.П.", 2, new int[]{4, 4, 5, 4 ,5}),
+            new Student("Сидорова Н.А.", 2, new int[]{3, 4, 5, 4 ,5}),
+            new Student("Петрова О.К.", 3, new int[]{5, 4, 5, 4 ,5}),
+            new Student("Алексеев А.А.", 3, new int[]{3, 4, 5, 4 ,5}),
+            new Student("Воронцова А.Н.", 3, new int[]{5, 5, 5, 4 ,5}),
+            new Student("Попов Д.А.", 4, new int[]{3, 4, 5, 4 ,5}),
+            new Student("Соколова П.С.", 4, new int[]{4, 4, 5, 4 ,5}),
+            new Student("Баранов Н.М.", 2, new int[]{4, 4, 5, 4 ,5}),
+            new Student("Климова И.Л.", 5, new int[]{4, 5, 5, 5 ,5}),
+
+};
+
+Array.Sort(students,
+           (s1, s2) => s1.AverageGrade.CompareTo(s2.AverageGrade));
+
+Console.WriteLine("Студенты с оценками только равными 4 или 5:");
+
+foreach (Student student in students)
+{
+    if (student.Grades.All(g => g == 4 || g == 5))
+    {
+        Console.WriteLine($"Фамилия: {student.FullName}, Номер группы: {student.GroupNumber}");
+    }
+}
+
---
+++ Lesson14(DZ)Методы расширения. Анонимные типы. Кортежи/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson14_DZ_Методы_расширения._Анонимные_типы._Кортежи_
+{
+    record Student(string FullName, int GroupNumber, int[] Grades)
+    {
+        public double AverageGrade => Grades.Average();
+    }
+}
