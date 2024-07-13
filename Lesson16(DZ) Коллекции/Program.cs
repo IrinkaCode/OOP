016cd0e4e60c01d27c77d80bf42814fa7ca8fb32
IrinkaCode <pil02022012@gmail.com>
2024-07-13 15:25:08 +0200

    1

--- Lesson16(DZ) Коллекции/Program.cs
+++ Lesson16(DZ) Коллекции/Program.cs
@@ -1,16 +1,18 @@
 ﻿//1. Массивы (Arrays): Создайте массив целых чисел и наполните его несколькими значениями от -10 до 10.
 
+using System.Collections.Generic;
+
 int[] numbers = new int[] { -5, 2, -8, 7, 0, -3, 9, -1, 4, -6 };
 
 //2. Списки (List): Создайте список строк и добавьте в него несколько элементов произвольных имен.
 
 List<string> names = new List<string>
 {
-    "John",
-    "Alice",
-    "Michael",
-    "Emily",
-    "David"
+    "Иван",
+    "Алиса",
+    "Михаил",
+    "Елена",
+    "Олег"
 };
 
 //3.Инициализация стека целых чисел и добавление нескольких элементов:
@@ -19,6 +21,11 @@ Stack<int> intStack = new Stack<int>();
 intStack.Push(10);
 intStack.Push(20);
 intStack.Push(30);
+foreach (int num in intStack)
+{
+    Console.WriteLine(num);
+}
+Console.WriteLine();
 
 //4.Демонстрация операций добавления и извлечения элементов из стека:
 Console.WriteLine("Элементы в стеке:");
@@ -30,6 +37,7 @@ foreach (int item in intStack)
 Console.WriteLine("\nИзвлечение элементов из стека:");
 Console.WriteLine(intStack.Pop()); // Извлечение и вывод последнего элемента
 Console.WriteLine(intStack.Peek()); // Вывод последнего элемента без извлечения
+Console.WriteLine();
 
 //5.Создание очереди строк и добавление в нее элементов:
 
@@ -48,7 +56,11 @@ if (ageDictionary.ContainsKey("Charlie"))
 {
     int charlieAge = ageDictionary["Charlie"]; 
 }
-
+foreach (KeyValuePair<string, int> pair in ageDictionary)
+{
+    Console.WriteLine($"Имя: {pair.Key}, Возраст: {pair.Value}");
+}
+Console.WriteLine();
 
 //7.Создание множества целых чисел и добавление в него элементов:
 
@@ -57,6 +69,11 @@ intSet.Add(10);
 intSet.Add(20);
 intSet.Add(30);
 intSet.Add(40);
+foreach (int num in intSet)
+{
+    Console.WriteLine(num);
+}
+Console.WriteLine();
 
 //8.Создание коллекции KeyValuePair, связывающей имена и возраст, и работа с ней:
 
