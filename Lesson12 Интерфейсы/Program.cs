e309ea2d9cbe0ebe5a1e6120b5d856812fe62278
IrinkaCode <pil02022012@gmail.com>
2024-07-17 22:58:54 +0200

    13,07

--- Lesson12 Интерфейсы/Program.cs
+++ Lesson12 Интерфейсы/Program.cs
@@ -12,31 +12,37 @@
 using Lesson12_Интерфейсы;
 
 TaskManager taskManager = new TaskManager();
-while(true)
+while (true)
 {
     Console.Clear();
     Console.WriteLine("Меню:\n" +
-        "1-Создать задачу\n" +
-        "2-Создать событие\n" +
-        "3- Удалить задачу или событие по названию");
-    Console.WriteLine("Поменять сортировку: 1 - по дате 2 - по приоритету");
-    int m;
-    int.TryParse(Console.ReadLine(), out m);
-    switch(m)
+        "1- Создать задачу\n" +
+        "2- Создать событие\n" +
+        "3- Удалить задачу или событие по названию" +
+        "4- Редактировать" +
+        "5- Сохранить" +
+        "6- Загрузить");
+    if (taskManager.Tasks.Count != 0 || taskManager.Events.Count != 0)
     {
-        case 1:
-            {
-                taskManager.Events.Sort();
-                taskManager.Tasks.Sort();
-            }
-            break;
+
+        Console.WriteLine("Поменять сортировку: 1 - по дате 2 - по приоритету");
+        int m;
+        int.TryParse(Console.ReadLine(), out m);
+        switch (m)
+        {
+            case 1:
+                {
+                    taskManager.Events.Sort();
+                    taskManager.Tasks.Sort();
+                }
+                break;
             case 2:
-            {
+                {
 
-            }
-            break;
+                }
+                break;
+        }
     }
-
     taskManager.Events.Sort();
     taskManager.Tasks.Sort(new TaskPriorityComparer());
 
@@ -44,17 +50,17 @@ while(true)
     Console.WriteLine("Выберите вариант: ");
     int n;
     int.TryParse(Console.ReadLine(), out n);
-    switch(n)
+    switch (n)
     {
-        case 1 :
+        case 1:
             {
                 Console.Write("Введите название задачи: ");
-                string name= Console.ReadLine()!;
+                string name = Console.ReadLine()!;
                 Console.Write("Введите дату задачи: ");
                 DateTime date = DateTime.Parse(Console.ReadLine()!);
                 Console.WriteLine("Введите приоритет: Low, Medium,High");
                 string priorityString = Console.ReadLine()!;
-                Priority priority=(Priority)Enum.Parse(typeof(Priority), priorityString);
+                Priority priority = (Priority)Enum.Parse(typeof(Priority), priorityString);
                 Lesson12_Интерфейсы.Task task = new Lesson12_Интерфейсы.Task
                 {
                     Name = name,
@@ -64,7 +70,7 @@ while(true)
                 taskManager.AddTask(task);
             }
             break;
-            case 2 :
+        case 2:
             {
                 Console.Write("Введите название задачи: ");
                 string name = Console.ReadLine()!;
@@ -85,19 +91,24 @@ while(true)
                 taskManager.AddEvent(@event);
             }
             break;
-            case 3 :
+        case 3:
             {
                 Console.Write("Введите название задачи: ");
                 string name = Console.ReadLine()!;
                 taskManager.RemoveTaskEvent(name);
             }
             break;
-            case 4 :
+        case 4:
             {
 
             }
             break;
-            case 5 :
+        case 5:
+            {
+
+            }
+            break;
+        case 6:
             {
 
             }
---
+++ Lesson13/Program.cs
@@ -0,0 +1,114 @@
+﻿// 
+using Lesson13;
+using System.Collections;
+
+//Person person = new Person { Name = "Alice", Age = 25 };
+//Console.WriteLine(person.Name);
+//Console.WriteLine(person.Age);
+//Person copyPerson = (Person)person.Clone();
+//copyPerson.Name = "Boris";
+//Console.WriteLine(copyPerson.Name);
+//Console.WriteLine(copyPerson.Age);
+
+
+//string[] peoples = ["Tom", "Sam", "jerry", "Bob"];
+//IEnumerator peopleEnumerator = peoples.GetEnumerator();
+//while (peopleEnumerator.MoveNext())
+//{
+//    string item = (string)peopleEnumerator.Current;
+//    Console.WriteLine(item);
+//}
+//peopleEnumerator.Reset();
+
+//Week week = new Week();
+//foreach (var day in week)
+//{
+//    Console.WriteLine(day);
+//}
+
+//Range range = 1..4;
+//int[] mas = { 7, 4, 8, 2 };
+//int[] masRange = mas[range];
+//foreach (var item in mas) Console.Write(item+",");
+//Console.WriteLine();
+
+//Numbers numbers = new Numbers();
+//foreach (var item in numbers)
+//{
+//    Console.Write(item+",");
+//}
+
+
+Del delegat = (x) =>
+{
+    Console.WriteLine(x);
+};
+delegat ("6");
+
+MesHello mes = () =>
+{
+    Console.WriteLine("Hello");
+};
+mes();
+
+//Operation operation = delegate (int x, int y)
+//{
+//    return x + y;
+//};
+//Console.WriteLine(operation(6,7));
+
+Operation operation = (x, y) => x + y;
+
+Console.WriteLine(operation(6, 7));
+
+operation = (x, y) => x * y;
+Console.WriteLine(operation(6, 7));
+
+int[] mas = { 1, 6, 3, 8, 9, 15 };
+Console.WriteLine(Sum(mas, x => x > 5));
+Console.WriteLine(Sum(mas, x => x % 2 == 0));
+IsOdd odd=(x)=> x % 2 == 0?true: false;
+Console.WriteLine(odd(6));
+
+Max max=(mas) => mas.Max();
+Console.WriteLine(max(mas));
+
+Sort sort=(mas)=> Array.Sort(mas);
+Console.WriteLine();
+
+Print print = (mas) =>
+{
+    foreach (var item in mas)
+    {
+        Console.Write(item + " ");
+    }
+};
+print(mas);
+
+Fact fact = (x) =>
+{
+    long f = 1;
+    for (int i = 1; i <= x; i++) f *= i;
+    return f;
+};
+Console.WriteLine(fact(5));
+
+int Sum(int[] ints, IsEqual fun)
+{
+    int sum = 0;
+    foreach (int x in ints)
+    {
+        if (fun(x)) sum += x;
+    }
+        return sum;
+}
+
+delegate bool IsOdd(int x);
+delegate bool IsEqual(int x);
+delegate void Del(string x);
+delegate void MesHello();
+delegate int Operation(int x, int y);
+delegate long Fact(long x);
+delegate int Max(int[]mas);
+delegate void Sort(int[]mas);
+delegate void Print (int[] mas);
