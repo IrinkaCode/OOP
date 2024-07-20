f80480622952b000af7e849955f184af8a631ebb
IrinkaCode <pil02022012@gmail.com>
2024-07-20 16:20:36 +0200

    1

--- Lesson18(DZ) Обобщения/Program.cs
+++ Lesson18(DZ) Обобщения/Program.cs
@@ -96,7 +96,7 @@ Console.WriteLine($"Возраст: {person.Age}");
 Console.WriteLine($"Пол: {person.Gender}");
 Console.WriteLine($"Вес: {person.Height}");
 Console.WriteLine($"Рост: {person.Weight}");
-Console.WriteLine($"Раса: {person.Gender}");
+Console.WriteLine($"Раса: {person.Race}");
 Console.WriteLine($"Цвет глаз: {person.EyeColor}");
 Console.WriteLine($"Адрес: {person.Address}");
 Console.WriteLine($"Номер телефона: {person.PhoneNumber}");
@@ -108,7 +108,7 @@ Console.WriteLine($"Возраст: {teacher.Age}");
 Console.WriteLine($"Пол: {teacher.Gender}");
 Console.WriteLine($"Вес: {teacher.Height}");
 Console.WriteLine($"Рост: {teacher.Weight}");
-Console.WriteLine($"Раса: {teacher.Gender}");
+Console.WriteLine($"Раса: {teacher.Race}");
 Console.WriteLine($"Цвет глаз: {teacher.EyeColor}");
 Console.WriteLine($"Адрес: {teacher.Address}");
 Console.WriteLine($"Номер телефона: {teacher.PhoneNumber}");
@@ -119,7 +119,56 @@ Console.WriteLine($"Зарплата: {teacher.Salary}");
 Console.WriteLine($"Позиция: {teacher.Position}");
 Console.WriteLine($"Дата приема на работу: {teacher.HireDate}");
 
+//Console.WriteLine();
 
+//Console.WriteLine($"Имя: {student.Name}");
+//Console.WriteLine($"Возраст: {student.Age}");
+//Console.WriteLine($"Пол: {student.Gender}");
+//Console.WriteLine($"Вес: {student.Height}");
+//Console.WriteLine($"Рост: {student.Weight}");
+//Console.WriteLine($"Раса: {student.Race}");
+//Console.WriteLine($"Цвет глаз: {student.EyeColor}");
+//Console.WriteLine($"Адрес: {student.Address}");
+//Console.WriteLine($"Номер телефона: {person.PhoneNumber}");
+//Console.WriteLine($"Курс: {student.Course}");
+//Console.WriteLine($"Специализация: {student.Specialization}");
+//Console.WriteLine($"Средний балл: {student.AverageGrade}");
 
+//Console.WriteLine();
 
+//Console.WriteLine($"Имя: {manager.Name}");
+//Console.WriteLine($"Возраст: {manager.Age}");
+//Console.WriteLine($"Пол: {manager.Gender}");
+//Console.WriteLine($"Вес: {manager.Height}");
+//Console.WriteLine($"Рост: {manager.Weight}");
+//Console.WriteLine($"Раса: {manager.Race}");
+//Console.WriteLine($"Цвет глаз: {manager.EyeColor}");
+//Console.WriteLine($"Адрес: {manager.Address}");
+//Console.WriteLine($"Номер сотрудника: {manager.EmployeeId}");
+//Console.WriteLine($"Номер телефона: {manager.PhoneNumber}");
+//Console.WriteLine($"Область управления: {manager.ManagementArea}");
+//Console.WriteLine($"Количество подчиненных: {manager.SubordinatesCount}");
+//Console.WriteLine($"Опыт управления: {manager.ManagementExperience}");
+//Console.WriteLine($"Зарплата: {manager.Salary}");
+//Console.WriteLine($"Позиция: {manager.Position}");
+//Console.WriteLine($"Дата приема на работу: {manager.HireDate}");
+
+//Console.WriteLine();
+
+//Console.WriteLine($"Имя: {programmer.Name}");
+//Console.WriteLine($"Возраст: {programmer.Age}");
+//Console.WriteLine($"Пол: {programmer.Gender}");
+//Console.WriteLine($"Вес: {programmer.Height}");
+//Console.WriteLine($"Рост: {programmer.Weight}");
+//Console.WriteLine($"Раса: {programmer.Race}");
+//Console.WriteLine($"Цвет глаз: {programmer.EyeColor}");
+//Console.WriteLine($"Адрес: {programmer.Address}");
+//Console.WriteLine($"Номер сотрудника: {programmer.EmployeeId}");
+//Console.WriteLine($"Номер телефона: {programmer.PhoneNumber}");
+//Console.WriteLine($"Язык программирования: {programmer.ProgrammingLanguage}");
+//Console.WriteLine($"Проекты: {programmer.Projects}");
+//Console.WriteLine($"Специализация: {programmer.Specialization}");
+//Console.WriteLine($"Зарплата: {programmer.Salary}");
+//Console.WriteLine($"Позиция: {programmer.Position}");
+//Console.WriteLine($"Дата приема на работу: {programmer.HireDate}");
 
