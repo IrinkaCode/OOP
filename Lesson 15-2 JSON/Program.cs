c57bef183128a2b9236bf07c024b8d1999ac6d67
IrinkaCode <pil02022012@gmail.com>
2024-07-27 18:38:13 +0200

    25.07

---
+++ Lesson 15-2 JSON/Program.cs
@@ -0,0 +1,70 @@
+﻿using Lesson_15_2_JSON;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+//var options = new JsonSerializerOptions
+//{
+//    WriteIndented = true,
+//    PropertyNamingPolicy=JsonNamingPolicy.CamelCase,
+//    DefaultIgnoreCondition= System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
+//};
+
+//Person person = new Person("Tom", 40);
+//string json = JsonSerializer.Serialize(person,options);
+//Console.WriteLine(json);
+
+//Person? restorePerson= JsonSerializer.Deserialize<Person>(json);
+//Console.WriteLine(restorePerson!.Name + " " + restorePerson.Age);
+
+//List<Person> persons = new List<Person>();
+//persons.Add(person);
+//persons.Add(new Person("Jerry",45));
+//persons.Add(new Person("Sam", 30));
+//persons.Add(new Person("Bill", 28));
+
+//string jsonList= JsonSerializer.Serialize(persons);
+//Console.WriteLine(jsonList);
+//List<Person>fronJson = JsonSerializer.Deserialize<List<Person>>(jsonList);
+//foreach(Person p in fronJson)
+//{
+//    Console.WriteLine(p!.Name + " " + p.Age);
+//}
+
+
+
+//class Person
+//{
+
+
+//    public Person(string name, int age)
+//    {
+//        Name = name;
+//        Age = age;
+//    }
+
+//    public string Name { get; set; }
+//    [JsonPropertyName("firstName")]
+//    public int Age { get; set; }
+// // [JsonIgnore]
+//}
+
+List<Product> products = new List<Product>
+{
+    new Product{Name="Хлеб",Count=20,Description="Белый",Price=40},
+        new Product{Name="Соль",Count=5,Description="Каменная",Price=10},
+            new Product{Name="Вода",Count=3,Description="Газированная",Price=20},
+};
+//string json = JsonSerializer.Serialize(products);
+//using (FileStream fs = new FileStream("products.json",FileMode.OpenOrCreate))
+//{
+//    await JsonSerializer.SerializeAsync(fs, products);
+//}
+using (FileStream fs = new FileStream("products.json", FileMode.OpenOrCreate))
+{
+    List<Product> list = await JsonSerializer.DeserializeAsync<List<Product>>(fs);
+    foreach (Product product in list)
+    {
+        Console.WriteLine(product.Name+" "+product.Description+" "+product.Count+" "+product.Price);
+    }
+}
---
+++ Lesson15 События/Program.cs
@@ -0,0 +1,55 @@
+﻿//События Events
+
+MyEvent myEvent = new MyEvent();
+UserInfo userInfo = new UserInfo(name: "Alex", family: "Petrov", age: 26);
+myEvent.UserEvent += userInfo.UserInfoHandler;
+myEvent.OnUserEvent();
+
+delegate void UI();
+
+class MyEvent
+{
+    UI[] evnt = new UI[5];
+    public event UI UserEvent 
+    {
+        add 
+        {
+            evnt[1] = value;
+        }
+        remove 
+        { 
+            evnt[1] = null;
+        }
+    }
+        
+    public void OnUserEvent()
+    {
+        //UserEvent();
+        evnt[1]();
+    }
+}
+
+class UserInfo
+{
+    public UserInfo(string? name, string family, int age)
+    {
+        Name = name;
+        Family = family;
+        Age = age;
+    }
+
+    public string? Name { get; set; }
+    public string Family { get; set; }
+    public int Age { get; set; }
+
+    public void UserInfoHandler()
+    {
+        Console.WriteLine("Событие вызвано");
+        Console.WriteLine($"Имя:{Name} Фамилия:{Family} Возраст:{Age}");
+    }
+}
+
+
+
+
+
