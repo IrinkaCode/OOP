5603ae442ebd8d05cf23cbf220913a7cc6d30802
IrinkaCode <pil02022012@gmail.com>
2024-08-02 22:49:59 +0200

    02.08.24

---
+++ Lesson23(DZ)События/Keyboard.cs
@@ -0,0 +1,12 @@
+﻿public class Keyboard
+{
+    public event EventHandler<char> KeyPressed;
+    public void SimulateKeyPress(char key)
+    {
+        OnKeyPressed(key);
+    }
+    protected virtual void OnKeyPressed(char key)
+    {
+        KeyPressed?.Invoke(this, key);
+    }
+}
---
+++ Lesson23(DZ)События/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задание: "Симулятор клавиатуры"
+//Это практическое задание направлено на создание симулятора клавиатуры с использованием событий в C#. Вам нужно создать класс Keyboard, который имеет событие KeyPressed. После этого вы должны реализовать методы для эмуляции нажатия клавиш клавиатуры и создать обработчик события KeyPressed, который будет выводить информацию о нажатой клавише.
+
+Keyboard keyboard = new Keyboard();
+keyboard.KeyPressed += Keyboard_KeyPressedHandler!;
+
+keyboard.SimulateKeyPress('A');
+keyboard.SimulateKeyPress('1');
+keyboard.SimulateKeyPress('Z');
+    
+static void Keyboard_KeyPressedHandler(object sender, char key)
+{
+    Console.WriteLine($"Клавиша '{key}' нажата.");
+}
