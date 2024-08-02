1c84886e368fa5038a159b23598dce7b7cc77cb1
IrinkaCode <pil02022012@gmail.com>
2024-08-02 22:35:16 +0200

    1

---
+++ Lesson23(DZ)События/Keyboard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson23_DZ_События
+{
+    public class Keyboard
+    {
+        public event EventHandler<char> KeyPressed;
+        public void SimulateKeyPress(char key)
+        {
+            OnKeyPressed(key);
+        }
+        protected virtual void OnKeyPressed(char key)
+        {
+            KeyPressed?.Invoke(this, key); 
+        }
+    }
+}
---
+++ Lesson23(DZ)События/Program.cs
@@ -0,0 +1,13 @@
+﻿using Lesson23_DZ_События;
+
+Keyboard keyboard = new Keyboard();
+keyboard.KeyPressed += Keyboard_KeyPressedHandler!;
+
+keyboard.SimulateKeyPress('A');
+keyboard.SimulateKeyPress('1');
+keyboard.SimulateKeyPress('Z');
+    
+static void Keyboard_KeyPressedHandler(object sender, char key)
+{
+    Console.WriteLine($"Клавиша '{key}' нажата.");
+}
