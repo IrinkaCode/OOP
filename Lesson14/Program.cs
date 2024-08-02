3581aa242d0fefb0de18e61b653e70d694b63ec7
IrinkaCode <pil02022012@gmail.com>
2024-08-02 21:31:00 +0200

    02/08

--- Lesson14/Program.cs
+++ Lesson14/Program.cs
@@ -1,25 +1,36 @@
-﻿//using Lesson14;
+﻿using Lesson14;
+using System.Threading.Channels;
 
-//IntOperation op = new IntOperation(Calculatior.Sum);
-
-//int x;                                                         //Дописать из гита
-//int.TryParse(
+//Console.Write("Введите первое число:");
+//int x;
+//int.TryParse(Console.ReadLine(), out x);
+//Console.Write("Введите первое число:");
 //int y;
-//IntOperation op = new IntOperation(Calculator.Sum);
-//Console.WriteLine($"{x}+{y}");
+//int.TryParse(Console.ReadLine(), out y);
 
+////IntOperation op = new IntOperation(Calculator.Sum);
+//IntOperation op = Calculator.Sum;
+//Console.WriteLine($"{x}+{y}={op(x,y)}");
+//op+=Calculator.Sub;
+//Console.WriteLine($"{x}-{y}={op(x, y)}");
+//op += Calculator.Mult;
+//Console.WriteLine($"{x}*{y}={op(x, y)}");
+//op += Calculator.Div;
+//Console.WriteLine($"{x}/{y}={op(x, y)}");
+//foreach (IntOperation del in op.GetInvocationList())
+//{
+//    Console.WriteLine(del(x, y));
+//}
 
-//using Lesson14;
-
-//UserInfo[] userInfo =
+//UserInfo[] userInfo = 
 //{
 //    new UserInfo("Иван","Иванов",900),
-//    new UserInfo("Алексей","Петров",120000),
-//    new UserInfo("Семен","Семенов",1800),
-//    new UserInfo("Петр","Петров",7900),
+//    new UserInfo("Алексей","Ерохин",120000),
+//    new UserInfo("Петр","Петров",8000),
+//    new UserInfo("Семен","Семенов",1900)
 //};
 //ArraySort.Sort(userInfo,UserInfo.UserSalary);
-//foreach(var item in userInfo)
+//foreach (var item in userInfo)
 //{
 //    Console.WriteLine(item);
 //}
@@ -29,43 +40,44 @@
 //    Console.WriteLine(item);
 //}
 
-//void DoOperation(int x, int y,Action<int,int>op)=>op(x,y);
+
+//void DoOparition(int x,int y,Action<int,int> op)=>op(x,y);
 //void Add(int x,int y)=>Console.WriteLine($"{x}+{y}={x+y}");
 //void Sub(int x, int y) => Console.WriteLine($"{x}-{y}={x - y}");
-//DoOperation(x, y, Add);
-//DoOperation(x, y, Sub);
+//DoOparition(x, y, Add);
+//DoOparition(x, y, Sub);
 
-//Predicate<int> IsPositiv = x => x > 0;
-//Console.WriteLine(IsPositiv(10));
-//Console.WriteLine(IsPositiv(-16));
+//Predicate<int> IsPositive = x => x > 0;
+//Console.WriteLine(IsPositive(10));
+//Console.WriteLine(IsPositive(-16));
 
-int[] mas= new int[10];
+
+//1
 Random random = new Random();
-for (int i = 0; i < mas.Length; i++)
-    mas[i] = random.Next(10,100);
-foreach (int i in mas)
-    Console.Write(i + " ");
+int[] mas = new int[10];
+for (int i = 0; i < mas.Length; i++) mas[i] = random.Next(10, 100);
+foreach (int i in mas) Console.Write(i + " ");
 Console.WriteLine();
 
-List<DateTime> tList = new List<DateTime>();
+//2
+List<DateTime> tList = new List<DateTime>()
 {
-    DateTime.Parse("2024-07-20");
-    DateTime.Parse("2024-07-28");
-    DateTime.Parse("2024-07-22");
-    DateTime.Parse("2024-07-17")
+    DateTime.Parse("2024-07-28"),
+    DateTime.Parse("2024-07-20"),
+    DateTime.Parse("2024-07-19"),
+    DateTime.Parse("2024-07-27"),
 };
-CheckPredicateDate(tList, x => x);
-
-
-void CheckPredicate(int[] x, Predicate<int>filter)
+CheckPredicatesDate(tList, x => x > DateTime.Now);
+void CheckPredicates(int[] x, Predicate<int> filter)
 {
-    for (int i = 0;i < mas.Length;i++)
+    for (int i = 0; i < mas.Length; i++)
     {
-        if (filter(x[i])) Console.Write(x[i]+" ");
+        if (filter(x[i])) Console.Write(x[i] + " ");
     }
     Console.WriteLine();
 }
-void CheckPredicateDate(List<DateTime> x, Predicate<DateTime> filter)
+
+void CheckPredicatesDate(List<DateTime> x, Predicate<DateTime> filter)
 {
     for (int i = 0; i < x.Count; i++)
     {
@@ -73,5 +85,4 @@ void CheckPredicateDate(List<DateTime> x, Predicate<DateTime> filter)
     }
     Console.WriteLine();
 }
-CheckPredicate(mas, x => x % 2 == 0);
-delegate bool FilterPredicate(int x);
+CheckPredicates(mas, x => x % 2 == 0);
---
+++ Lesson21(DZ)Анон.методы,лямбда/Program.cs
@@ -0,0 +1,56 @@
+﻿//Задача 1. Создайте анонимный метод, который принимает два аргумента(числа) и возвращает их сумму. Затем создайте делегат, который может вызывать этот анонимный метод, и используйте его для выполнения сложения чисел.
+//Задача 2. Создайте массив строк и отфильтруйте его с использованием лямбда-выражения. Отфильтруйте строки, чтобы получить только те,которые начинаются с определенной буквы (например, "A").
+//Задача 3. Создайте массив делегатов, представляющих различные математические операции (сложение, вычитание, умножение, деление).Запросите у пользователя числа и операцию, которую он хочет выполнить.Затем используйте массив делегатов, чтобы выполнить выбранную операцию с введенными числами.
+
+//Задача 1
+//SumDelegate sum = delegate (int x, int y)
+//{
+//    return x + y;
+//};
+//int result = sum(5, 3);
+//Console.WriteLine("Сумма чисел: " + result);
+//delegate int SumDelegate(int a, int b);
+
+//Задача 2
+//string[] words = { "Apple", "Banana", "Orange", "Avocado", "Apricot", "Pear" };
+//char startingLetter = 'A';
+//Console.WriteLine($"Строки, начинающиеся с буквы '{startingLetter}':");
+//var filteredWords = words.Where(word => word.StartsWith(startingLetter.ToString(), StringComparison.OrdinalIgnoreCase));
+//foreach (var word in filteredWords)
+//{
+//    Console.WriteLine(word);
+//}
+
+
+//Задача 3
+Console.Write("Введите первое число: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Выберите операцию:");
+Console.WriteLine("1. Сложение");
+Console.WriteLine("2. Вычитание");
+Console.WriteLine("3. Умножение");
+Console.WriteLine("4. Деление");
+
+int choice = Convert.ToInt32(Console.ReadLine());
+
+if (choice < 1 || choice > 4)
+{
+    Console.WriteLine("Неверный выбор операции.");
+    return;
+}
+
+MathOperation[] operations = {
+            (x, y) => x + y, // сложение
+            (x, y) => x - y, // вычитание
+            (x, y) => x * y, // умножение
+            (x, y) => x / y  // деление
+        };
+
+double result = operations[choice - 1](num1, num2);
+Console.WriteLine($"Результат операции: {result}");
+delegate double MathOperation(double x, double y);
+
+
