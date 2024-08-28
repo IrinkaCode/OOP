71bb44169c31943c602104441db42c01ec8b193c
IrinkaCode <pil02022012@gmail.com>
2024-08-28 23:41:27 +0200

    28,08

--- Lesson16 DeskTop/ReaderWindow.Designer.cs
+++ Lesson16 DeskTop/ReaderWindow.Designer.cs
@@ -28,38 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textTitle = new TextBox();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
             textBox3 = new TextBox();
             SuspendLayout();
-            // 
-            // textTitle
-            // 
-            textTitle.Location = new Point(50, 49);
-            textTitle.Multiline = true;
-            textTitle.Name = "textTitle";
-            textTitle.PlaceholderText = "Заголовок";
-            textTitle.Size = new Size(410, 34);
-            textTitle.TabIndex = 0;
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(50, 116);
-            textBox1.Multiline = true;
-            textBox1.Name = "textBox1";
-            textBox1.PlaceholderText = "Тема";
-            textBox1.Size = new Size(410, 34);
-            textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(47, 190);
-            textBox2.Multiline = true;
-            textBox2.Name = "textBox2";
-            textBox2.PlaceholderText = "Возрастное ограничение";
-            textBox2.Size = new Size(413, 34);
-            textBox2.TabIndex = 2;
             // 
             // textBox3
             // 
@@ -76,9 +46,6 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(501, 605);
             Controls.Add(textBox3);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
-            Controls.Add(textTitle);
             Name = "ReaderWindow";
             Text = "ReaderWindow";
             Load += ReaderWindow_Load;
@@ -87,10 +54,6 @@
         }
 
         #endregion
-
-        private TextBox textTitle;
-        private TextBox textBox1;
-        private TextBox textBox2;
         private TextBox textBox3;
     }
 }
