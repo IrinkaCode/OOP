30c3ad4c82359d3cc711141b3fc22ccad611bfad
IrinkaCode <pil02022012@gmail.com>
2024-11-26 09:58:54 +0200

    2

--- Tema28.1(DZ) EPPlus/Program.cs
+++ Tema28.1(DZ) EPPlus/Program.cs
@@ -23,63 +23,6 @@
 //Создайте в нем лист "Клиенты" и запишите в него набор данных из массива клиентов.
 //Затем сохраните файл на диске.
 
-//using OfficeOpenXml;
-//using System.Diagnostics.Metrics;
-//using System.Drawing.Drawing2D;
-
-//Client[] mas =
-//{
-//    new Client("Иван", "Иванов", "ivan@example.com"),
-//    new Client("Петр", "Петров", "peter@example.com"),
-//    new Client("Мария", "Сидорова", "maria@example.com"),
-//    new Client("Анна", "Кузнецова", "anna@example.com"),
-//    new Client("Алексей", "Смирнов", "alex@example.com"),
-//    new Client("Наталья", "Васильева", "natalia@example.com"),
-//    new Client("Сергей","Морозов", "sergey@example.com"),
-//    new Client("Ольга", "Павлова", "olga@example.com"),
-//    new Client("Денис", "Жуков", "denis@example.com"),
-//    new Client("Екатерина", "Макарова", "ekaterina@example.com")
-
-//};
-
-//FileInfo fileInfo = new FileInfo("Клиенты.xlsx");
-//if (!fileInfo.Exists) fileInfo.Create();
-//ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
-//ExcelPackage newBook = new ExcelPackage("Клиенты.xlsx");
-
-//newBook.Workbook.Worksheets.Add("Клиенты");
-//ExcelWorksheet worksheet = newBook.Workbook.Worksheets["Клиенты"];
-
-//worksheet.Cells["A1"].Value = "Имя";
-//worksheet.Cells["B1"].Value = "Фамилия";
-//worksheet.Cells["C1"].Value = "Email";
-
-//for (int i = 0; i < mas.Length; i++)
-//{
-//    worksheet.Cells[2 + i, 1].Value = mas[i].Name;
-//    worksheet.Cells[2 + i, 2].Value = mas[i].LastName;
-//    worksheet.Cells[2 + i, 3].Value = mas[i].Email;
-
-//}
-
-
-//newBook.Save();
-////newBook.SaveAs(fileInfo);
-//Console.WriteLine("Таблица создана");
-
-//class Client
-//{
-//    public string? Name { get; set; }
-//    public string? LastName { get; set; }
-//    public string? Email { get; set; }
-//    public Client(string? name, string? lastName, string? email)
-//    {
-//        Name = name;
-//        LastName = lastName;
-//        Email = email;
-//    }
-
-//}
 
 using OfficeOpenXml;
 using OfficeOpenXml.Style;
