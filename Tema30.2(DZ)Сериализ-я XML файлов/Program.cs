9ee28136b71614fab45bc53ff3c1ac5f000cfeeb
IrinkaCode <pil02022012@gmail.com>
2024-12-23 15:13:22 +0200

    23/12

--- Tema30.2(DZ)Сериализ-я XML файлов/Program.cs
+++ Tema30.2(DZ)Сериализ-я XML файлов/Program.cs
@@ -5,126 +5,13 @@
 //● Также должна быть возможность сохранить данные пользователей.
 //● Должна быть возможность вывести сохраненные данные пользователей на консоль используя десериализацию, цикл foreach и так далее.
 
-//using System;
-//using System.Collections.Generic;
-//using System.IO;
-//using System.Xml.Serialization;
-//using Tema30._2_DZ_Сериализ_я_XML_файлов;
 
-//class Program
-//{
-//    private const string FilePath = "users.xml";
-//    private static List<User> users = new List<User>();
-
-//    static void Main(string[] args)
-//    {
-//        LoadUsers();
-
-//        while (true)
-//        {
-//            Console.WriteLine("1. Регистрация");
-//            Console.WriteLine("2. Аутентификация");
-//            Console.WriteLine("3. Вывести всех пользователей");
-//            Console.WriteLine("4. Выход");
-//            var choice = Console.ReadLine();
-
-//            switch (choice)
-//            {
-//                case "1":
-//                    RegisterUser();
-//                    break;
-//                case "2":
-//                    AuthenticateUser();
-//                    break;
-//                case "3":
-//                    DisplayUsers();
-//                    break;
-//                case "4":
-//                    SaveUsers();
-//                    return;
-//                default:
-//                    Console.WriteLine("Неверный выбор. Попробуйте еще раз.");
-//                    break;
-//            }
-//        }
-//    }
-
-//    private static void LoadUsers()
-//    {
-//        if (File.Exists(FilePath))
-//        {
-//            using (var stream = new FileStream(FilePath, FileMode.Open))
-//            {
-//                XmlSerializer serializer = new XmlSerializer(typeof(List<User>));
-//                users = (List<User>)serializer.Deserialize(stream);
-//            }
-//        }
-//    }
-
-//    private static void SaveUsers()
-//    {
-//        using (var stream = new FileStream(FilePath, FileMode.Create))
-//        {
-//            XmlSerializer serializer = new XmlSerializer(typeof(List<User>));
-//            serializer.Serialize(stream, users);
-//        }
-//    }
-
-//    private static void RegisterUser()
-//    {
-//        Console.Write("Введите имя: ");
-//        var name = Console.ReadLine();
-
-//        Console.Write("Введите возраст: ");
-//        if (int.TryParse(Console.ReadLine(), out int age))
-//        {
-//            var user = new User { Name = name, Age = age };
-//            users.Add(user);
-//            Console.WriteLine("Пользователь успешно зарегистрирован!");
-//        }
-//        else
-//        {
-//            Console.WriteLine("Неверный возраст. Попробуйте еще раз.");
-//        }
-//    }
-
-//    private static void AuthenticateUser()
-//    {
-//        Console.Write("Введите имя для аутентификации: ");
-//        var name = Console.ReadLine();
-
-//        var user = users.FirstOrDefault(u => u.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
-//        if (user != null)
-//        {
-//            Console.WriteLine($"Аутентификация успешна! Имя: {user.Name}, Возраст: {user.Age}");
-//        }
-//        else
-//        {
-//            Console.WriteLine("Пользователь не найден.");
-//        }
-//    }
-
-//    private static void DisplayUsers()
-//    {
-//        if (users.Count == 0)
-//        {
-//            Console.WriteLine("Пользователи не найдены.");
-//            return;
-//        }
-
-//        Console.WriteLine("Список зарегистрированных пользователей:");
-//        foreach (var user in users)
-//        {
-//            Console.WriteLine($"Имя: {user.Name}, Возраст: {user.Age}");
-//        }
-
-//    }
-//}
 
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
+using Tema30._2_DZ_Сериализ_я_XML_файлов;
 
 UserManager userManager = new UserManager();
 userManager.LoadUsers();
@@ -157,7 +44,7 @@ while (true)
 
         case "2":
             Console.Write("Введите имя для аутентификации: ");
-            string authName = Console.ReadLine();
+            string authName = Console.ReadLine()!;
             if (userManager.AuthenticateUser(authName))
             {
                 Console.WriteLine("Аутентификация успешна.");
@@ -181,64 +68,58 @@ while (true)
     }
 }
 
-[Serializable]
-public class User
-{
-    public string? Name { get; set; }
-    public int Age { get; set; }
-}
 
-public class UserManager
-{
-    private List<User> users = new List<User>();
-    private const string filePath = "users.xml";
+//public class UserManager
+//{
+//    private List<User> users = new List<User>();
+//    private const string filePath = "users.xml";
 
-    public void LoadUsers()
-    {
-        if (File.Exists(filePath))
-        {
-            XmlSerializer serializer = new XmlSerializer(typeof(List<User>));
-            using (FileStream fs = new FileStream(filePath, FileMode.Open))
-            {
-                users = (List<User>)serializer.Deserialize(fs);
-            }
-        }
-    }
+//    public void LoadUsers()
+//    {
+//        if (File.Exists(filePath))
+//        {
+//            XmlSerializer serializer = new XmlSerializer(typeof(List<User>));
+//            using (FileStream fs = new FileStream(filePath, FileMode.Open))
+//            {
+//                users = (List<User>)serializer.Deserialize(fs);
+//            }
+//        }
+//    }
 
-    public void SaveUsers()
-    {
-        XmlSerializer serializer = new XmlSerializer(typeof(List<User>));
-        using (FileStream fs = new FileStream(filePath, FileMode.Create))
-        {
-            serializer.Serialize(fs, users);
-        }
-    }
+//    public void SaveUsers()
+//    {
+//        XmlSerializer serializer = new XmlSerializer(typeof(List<User>));
+//        using (FileStream fs = new FileStream(filePath, FileMode.Create))
+//        {
+//            serializer.Serialize(fs, users);
+//        }
+//    }
 
-    public void RegisterUser(string name, int age)
-    {
-        if (users.Exists(u => u.Name.Equals(name, StringComparison.OrdinalIgnoreCase)))
-        {
-            Console.WriteLine("Пользователь с таким именем уже существует.");
-            return;
-        }
+//    public void RegisterUser(string name, int age)
+//    {
+//        if (users.Exists(u => u.Name.Equals(name, StringComparison.OrdinalIgnoreCase)))
+//        {
+//            Console.WriteLine("Пользователь с таким именем уже существует.");
+//            return;
+//        }
 
-        users.Add(new User { Name = name, Age = age });
-        Console.WriteLine("Пользователь успешно зарегистрирован.");
-    }
+//        users.Add(new User { Name = name, Age = age });
+//        Console.WriteLine("Пользователь успешно зарегистрирован.");
+//    }
 
-    public bool AuthenticateUser(string name)
-    {
-        return users.Exists(u => u.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
-    }
+//    public bool AuthenticateUser(string name)
+//    {
+//        return users.Exists(u => u.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+//    }
 
-    public void DisplayUsers()
-    {
-        foreach (var user in users)
-        {
-            Console.WriteLine($"Имя: {user.Name}, Возраст: {user.Age}");
-        }
-    }
-}
+//    public void DisplayUsers()
+//    {
+//        foreach (var user in users)
+//        {
+//            Console.WriteLine($"Имя: {user.Name}, Возраст: {user.Age}");
+//        }
+//    }
+//}
 
 
 
---
+++ Tema30.2(DZ)Сериализ-я XML файлов/UserManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Tema30._2_DZ_Сериализ_я_XML_файлов
+{
+    public class UserManager
+    {
+        private List<User> users = new List<User>();
+        private const string filePath = "users.xml";
+
+        public void LoadUsers()
+        {
+            if (File.Exists(filePath))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<User>));
+                using (FileStream fs = new FileStream(filePath, FileMode.Open))
+                {
+                    users = (List<User>)serializer.Deserialize(fs);
+                }
+            }
+        }
+
+        public void SaveUsers()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<User>));
+            using (FileStream fs = new FileStream(filePath, FileMode.Create))
+            {
+                serializer.Serialize(fs, users);
+            }
+        }
+
+        public void RegisterUser(string name, int age)
+        {
+            if (users.Exists(u => u.Name.Equals(name, StringComparison.OrdinalIgnoreCase)))
+            {
+                Console.WriteLine("Пользователь с таким именем уже существует.");
+                return;
+            }
+
+            users.Add(new User { Name = name, Age = age });
+            Console.WriteLine("Пользователь успешно зарегистрирован.");
+        }
+
+        public bool AuthenticateUser(string name)
+        {
+            return users.Exists(u => u.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public void DisplayUsers()
+        {
+            foreach (var user in users)
+            {
+                Console.WriteLine($"Имя: {user.Name}, Возраст: {user.Age}");
+            }
+        }
+    }
+}
