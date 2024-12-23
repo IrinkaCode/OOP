edb0e00b00c802a5f8cc726641135eb494dfb5bb
IrinkaCode <pil02022012@gmail.com>
2024-12-23 15:14:22 +0200

    1

--- Tema30.2(DZ)Сериализ-я XML файлов/Program.cs
+++ Tema30.2(DZ)Сериализ-я XML файлов/Program.cs
@@ -69,57 +69,7 @@ while (true)
 }
 
 
-//public class UserManager
-//{
-//    private List<User> users = new List<User>();
-//    private const string filePath = "users.xml";
 
-//    public void LoadUsers()
-//    {
-//        if (File.Exists(filePath))
-//        {
-//            XmlSerializer serializer = new XmlSerializer(typeof(List<User>));
-//            using (FileStream fs = new FileStream(filePath, FileMode.Open))
-//            {
-//                users = (List<User>)serializer.Deserialize(fs);
-//            }
-//        }
-//    }
-
-//    public void SaveUsers()
-//    {
-//        XmlSerializer serializer = new XmlSerializer(typeof(List<User>));
-//        using (FileStream fs = new FileStream(filePath, FileMode.Create))
-//        {
-//            serializer.Serialize(fs, users);
-//        }
-//    }
-
-//    public void RegisterUser(string name, int age)
-//    {
-//        if (users.Exists(u => u.Name.Equals(name, StringComparison.OrdinalIgnoreCase)))
-//        {
-//            Console.WriteLine("Пользователь с таким именем уже существует.");
-//            return;
-//        }
-
-//        users.Add(new User { Name = name, Age = age });
-//        Console.WriteLine("Пользователь успешно зарегистрирован.");
-//    }
-
-//    public bool AuthenticateUser(string name)
-//    {
-//        return users.Exists(u => u.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
-//    }
-
-//    public void DisplayUsers()
-//    {
-//        foreach (var user in users)
-//        {
-//            Console.WriteLine($"Имя: {user.Name}, Возраст: {user.Age}");
-//        }
-//    }
-//}
 
 
 
