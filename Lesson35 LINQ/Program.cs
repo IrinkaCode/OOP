bd02e20b0503a1ad2deff8ac2a8504d6a8e0256b
IrinkaCode <pil02022012@gmail.com>
2025-02-04 01:23:18 +0200

    03,01

---
+++ Lesson35 LINQ/Program.cs
@@ -0,0 +1,62 @@
+﻿//LINQ
+
+
+using System.Linq;
+
+List<Car>GetCars()
+{
+    return new List<Car>
+    {
+        new Car { VIN="ABC123",Make="Ford",Model= "F-259",Year=2000 },
+        new Car { VIN="ABC456",Make="VW",Model= "F-2598",Year=2003 },
+        new Car { VIN="ABC789",Make="BMW",Model= "E39",Year=1995 },
+        new Car { VIN="CDE123",Make="Audi",Model= "F-257",Year=1989 },
+        new Car { VIN="ABC456",Make="Tesla",Model= "F4567",Year=2020 },
+    };
+}
+
+bool result = GetCars().All(c => c.Year > 1960);
+Console.WriteLine(result);
+result = GetCars().Any(c => c.Year > 1960);
+Console.WriteLine(result);
+
+var cars = GetCars();
+//var carsByVin = cars.ToDictionary(c => c.VIN);
+//Console.WriteLine(carsByVin["ABC456"].Make);
+
+var carsLookUp = cars.ToLookup(c => c.Make);
+IEnumerable<Car> list = carsLookUp["Ford"];
+foreach (var car in list)
+{
+    Console.WriteLine(car.Year);
+}
+
+var numbers = Enumerable.Range(1, 1000);
+//foreach(var car in numbers)
+//{
+//    Console.WriteLine(car);
+//}
+
+var zip = numbers.Zip(cars, (i, c) => new
+{
+    Number = i,
+    CarMake = c.Make
+});
+foreach (var car in zip)
+{
+    Console.WriteLine(car);
+}
+
+
+
+
+class Car
+{
+    public string VIN { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; }
+}
+
+
---
+++ Tema35-1(DZ)Введ-е в LINQ/Program.cs
@@ -0,0 +1,66 @@
+﻿//Вам дается CSV-файл InventoryControl.csv
+//Считайте его данные в список объектов класса Good (создайте его) и, используя LINQ, только те товары, количество которых - четное
+
+////1 вариант
+//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+
+//string filePath = @"C:\Users\pil02\Desktop\inventoryControl.csv";
+
+//var lines = File.ReadAllLines(filePath);
+
+//// Преобразуем строки в список объектов Good
+//var goods = lines.Skip(1) 
+//                 .Select(line => line.Split(','))
+//                 .Select(parts => new Good
+//                 {
+//                     Name = parts[0],
+//                     Quantity = int.Parse(parts[1])
+//                 })
+//                 .ToList();
+
+//var evenQuantityGoods = goods.Where(g => g.Quantity % 2 == 0);
+
+//foreach (var good in evenQuantityGoods)
+//{
+//    Console.WriteLine($"Name: {good.Name}, Quantity: {good.Quantity}");
+//}
+
+//class Good
+//{
+//    public string? Name { get; set; }
+//    public int Quantity { get; set; }
+//}
+
+
+////2 вариант
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using System.Linq;
+
+string filePath = @"C:\Users\pil02\Desktop\inventoryControl.csv";
+List<Good> goods;
+
+using (var reader = new StreamReader(filePath))
+using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+{
+    goods = csv.GetRecords<Good>().ToList();
+}
+
+// Выводим товары с четным количеством
+var evenQuantityGoods = goods.Where(g => g.Quantity % 2 == 0);
+foreach (var good in evenQuantityGoods)
+{
+    Console.WriteLine($"Name: {good.Name}, Quantity: {good.Quantity}");
+}
+
+public class Good
+{
+    public string? Name { get; set; }
+    public int Quantity { get; set; }
+}
