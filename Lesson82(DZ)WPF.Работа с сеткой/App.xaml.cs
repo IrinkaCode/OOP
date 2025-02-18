8b1e512dbb8fbf0af42d3621116313d6418278d4
IrinkaCode <pil02022012@gmail.com>
2025-02-18 23:39:00 +0200

    les82

---
+++ Lesson82(DZ)WPF.Работа с сеткой/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Lesson82_DZ_WPF.Работа_с_сеткой;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
