e659a4ffd4a2a401f1c27523c2bee26b19d41608
IrinkaCode <pil02022012@gmail.com>
2025-02-18 20:47:28 +0200

    18.02

---
+++ Lesson81(DZ)WPF.Знакомство с XAML/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Lesson81_DZ_WPF.Знакомство_с_XAML;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
